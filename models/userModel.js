c73fb83857a3bc8ccc7867dc40bf5c1d2ea9fe8a
niloysaha06 <niloysaha895@gmail.com>
2023-06-12 22:23:15 +0600

    create userModel

---
+++ models/userModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true, //remove whitespace
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true, //unique email
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: Number,
+      default: 0, //0=false,1=true
+    },
+  },
+  { timestamps: true } //when new user create,added created time
+);
+
+export default mongoose.model("users", userSchema);
