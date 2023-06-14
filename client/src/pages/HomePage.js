c1d675f0f27ead7b1f7418cdea0b3bd98e1c3fda
niloysaha06 <niloysaha895@gmail.com>
2023-06-14 17:35:50 +0600

    create clientsite

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+const HomePage = () => {
+  return <Layout>HomePage</Layout>;
+};
+
+export default HomePage;
---
+++ client/src/pages/PageNotFound.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+const PageNotFound = () => {
+  return <Layout>PageNotFound</Layout>;
+};
+
+export default PageNotFound;
