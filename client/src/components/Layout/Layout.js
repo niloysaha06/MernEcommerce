f3335bdf9508b6b64b37679fba8fdab8c41a7074
niloysaha06 <niloysaha895@gmail.com>
2023-06-15 13:07:54 +0600

    SEO

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Header from "./Header";
 import Footer from "./Footer";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <>
       <Header />
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
+
       <main
         style={{
           minHeight: `calc(100vh - 195px)`,
@@ -16,6 +25,13 @@ const Layout = ({ children }) => {
       <Footer />
     </>
   );
+};
+
+Layout.defaultProps = {
+  title: "Ecommerce App -Shop Now",
+  description: "Mern stack project",
+  keywords: "mern,react,node,mongodb",
+  author: "Niloy",
 };
 
 export default Layout;
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "../components/Layout/Layout";
 
 const HomePage = () => {
-  return <Layout>HomePage</Layout>;
+  return <Layout title="Best Offers-Shop Now">HomePage</Layout>;
 };
 
 export default HomePage;
