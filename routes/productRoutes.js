fe08b95c1ab82bbf2735ad0387dbaeee77223e2e
niloysaha06 <niloysaha895@gmail.com>
2023-06-20 21:53:38 +0600

    product update and delete(server)

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,13 @@
 import express from "express";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
-import { createProductController } from "../controllers/productController.js";
+import {
+  createProductController,
+  deleteController,
+  getProductController,
+  getSingleProductController,
+  productPhotoController,
+  updateProductController,
+} from "../controllers/productController.js";
 import formidable from "express-formidable";
 
 const router = express.Router();
@@ -12,6 +19,27 @@ router.post(
   isAdmin,
   formidable(),
   createProductController
+);
+
+//get products
+router.get("/get-product", getProductController);
+
+//single product
+router.get("/get-product/:slug", getSingleProductController);
+
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
+
+//delete product
+router.delete("/product/:pid", deleteController);
+
+//update product
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
 );
 
 export default router;
