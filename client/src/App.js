390500e0b9370a3f14b4bbf16efa3c3ef884d491
niloysaha06 <niloysaha895@gmail.com>
2023-06-22 23:03:05 +0600

    Get all product

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import CreateProduct from "./pages/Admin/CreateProduct";
 import Users from "./pages/Admin/Users";
 import Profile from "./pages/user/Profile";
 import Orders from "./pages/user/Orders";
+import Products from "./pages/Admin/Products";
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
           <Route path="admin/create-category" element={<CreateCategory />} />
           <Route path="admin/create-product" element={<CreateProduct />} />
           <Route path="admin/users" element={<Users />} />
+          <Route path="admin/products" element={<Products />} />
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
