8be90c52c97845617a2be5be3aea988782161845
niloysaha06 <niloysaha895@gmail.com>
2023-06-24 00:59:56 +0600

    Filter categories and price functionality

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -4,10 +4,30 @@ import { useAuth } from "../context/auth";
 import { toast } from "react-hot-toast";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { Checkbox, Radio } from "antd";
+import { Prices } from "../components/Prices";
 
 const HomePage = () => {
   const [products, setProducts] = useState("");
   const [categories, setCategories] = useState("");
+  const [checked, setChecked] = useState([]);
+  const [radio, setRadio] = useState([]);
+
+  //get all category
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/category/get-category");
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
 
   //get all products
   const getAllProducts = async () => {
@@ -24,11 +44,46 @@ const HomePage = () => {
     getAllProducts();
   }, []);
 
+  //filter by category
+  const handleFilter = async (value, id) => {
+    let all = [...checked];
+    if (value) {
+      all.push(id);
+    } else {
+      all = all.filter((c) => c !== id);
+    }
+    setChecked(all);
+  };
+
   return (
     <Layout title="Best Offers-Shop Now">
       <div className="row mt-3">
-        <div className="col-md-3">
+        <div className="col-md-2 ms-2">
           <h4 className="text-center">Filter By Category</h4>
+          <div className="d-flex flex-column">
+            {categories &&
+              categories.map((c) => (
+                <Checkbox
+                  key={c._id}
+                  onChange={(e) => handleFilter(e.target.checked, c._id)}
+                >
+                  {c.name}
+                </Checkbox>
+              ))}
+          </div>
+          <div className="mt-3">
+            <h4 className="text-center">Filter By Price</h4>
+            <div className="d-flex flex-column">
+              <Radio.Group onChange={(e) => setRadio(e.target.value)}>
+                {Prices &&
+                  Prices?.map((p) => (
+                    <div key={p.id}>
+                      <Radio value={p.array}>{p?.name}</Radio>
+                    </div>
+                  ))}
+              </Radio.Group>
+            </div>
+          </div>
         </div>
         <div className="col-md-9">
           <h1 className="text-center">All Products</h1>
