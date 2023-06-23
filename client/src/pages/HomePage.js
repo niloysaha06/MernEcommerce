3c29ac62542621c1a34747ed638c1c1c06527f94
niloysaha06 <niloysaha895@gmail.com>
2023-06-23 18:28:25 +0600

    Homepage get products

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout/Layout";
 import { useAuth } from "../context/auth";
+import { toast } from "react-hot-toast";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  const [auth, setAuth] = useAuth();
+  const [products, setProducts] = useState("");
+  const [categories, setCategories] = useState("");
+
+  //get all products
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/product/get-product");
+      setProducts(data?.products);
+    } catch (error) {
+      console.log(error);
+      toast.error("Somethings went wrong");
+    }
+  };
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
   return (
     <Layout title="Best Offers-Shop Now">
-      <h1>Homepage</h1>
-      <pre>{JSON.stringify(auth, null, 4)}</pre>
+      <div className="row mt-3">
+        <div className="col-md-3">
+          <h4 className="text-center">Filter By Category</h4>
+        </div>
+        <div className="col-md-9">
+          <h1 className="text-center">All Products</h1>
+          <div className="d-flex flex-wrap">
+            {products &&
+              products?.map((p) => (
+                <div className="card m-2" style={{ width: "18rem" }}>
+                  <img
+                    src={`/api/v1/product/product-photo/${p._id}`}
+                    className="card-img-top"
+                    alt="..."
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{p?.name}</h5>
+                    <p className="card-text">{p?.description}</p>
+                    <button className="btn btn-primary ms-1">
+                      More Details
+                    </button>
+                    <button className="btn btn-secondary ms-1">
+                      ADD TO CART
+                    </button>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
