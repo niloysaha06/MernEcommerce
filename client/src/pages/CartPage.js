47ce6d9da72f11a1a8e0f1b3653790d57f69ea34
niloysaha06 <niloysaha895@gmail.com>
2023-08-19 18:39:04 +0600

    Item add to cart

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -8,6 +8,29 @@ const CartPage = () => {
   const [auth, setAuth] = useAuth();
   const [cart, setCart] = useCart();
   const navigate = useNavigate();
+
+  // remove item
+  const removeCartItem = (pid) => {
+    try {
+      let myCart = [...cart];
+      let index = myCart.findIndex((item) => item._id === pid);
+      myCart.splice(index, 1);
+      setCart(myCart);
+      localStorage.setItem("cart", JSON.stringify(myCart));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // const removeCartItem = (pid) => {
+  //   try {
+  //     let myCart = [...cart];
+  //     let index = myCart.filter((item) => item._id !== pid);
+  //     setCart(index);
+  //    localStorage.setItem("cart", JSON.stringify(myCart));
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
   return (
     <Layout>
       <div className="container">
@@ -17,12 +40,48 @@ const CartPage = () => {
               {`Hello ${auth?.token && auth?.user?.name}`}
             </h1>
             <h4 className="text-center">
-              {cart?.length >= 1
+              {cart?.length
                 ? `You have ${cart?.length} items in your cart ${
                     auth?.token ? "" : "Please login to checkout"
                   } `
                 : "Your Cart is Empty"}
             </h4>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-8">
+            <div>
+              {cart?.map((p) => (
+                <div className="row mb-2 p-3 card flex-row">
+                  <div className="col-md-4">
+                    <img
+                      src={`/api/v1/product/product-photo/${p?._id}`}
+                      className="card-img-top"
+                      alt="..."
+                      width={"100px"}
+                      height={"150px"}
+                    />
+                  </div>
+                  <div className="col-md-8">
+                    <p>{p?.name}</p>
+                    <p>{p?.description.substring(0, 30)}</p>
+                    <h4>Praice : {p?.price}</h4>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => removeCartItem(p?._id)}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="col-md-4 text-center">
+            <h2>Cart Summary</h2>
+            <hr />
+            <p>Total | Checkout | Payment</p>
+            <h4>Total : </h4>
           </div>
         </div>
       </div>
